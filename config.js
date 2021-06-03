a3f9207e29d0958cc5fece4ab6d1ab49bcebfc3b
hieutobi1 <85281953+hieutobi1@users.noreply.github.com>
2021-06-03 15:34:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Hà à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao thế giới đẻ đc đc như bạn
+    Ăn gì mà ngu vậy bạn
+    sống bới ngu nha bạn`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Hà quá ngu 😙',
+    desc: 'Phải chăng Hà mới sinh ra đã ngu ',
+    btnYes: 'Vâng tớ ngu <33',
+    btnNo: 'Không,Hà không ngu :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Hà ngu vl <3',
+    btnReply: 'Hà Ngu <3',
+    reply: 'đứa ngu nhất thế giới là Hà <33333333',
+    mess: 'Tớ biết mà 🥰. Hà ngu 😘😘',
+    messDesc: 'Tối nay 8h thi thử thầy đạt nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
